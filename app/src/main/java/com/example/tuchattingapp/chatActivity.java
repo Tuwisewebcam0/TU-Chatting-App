04b4208fed11f0ac868720bc17bfd2474590cb0f
Egharevba Nosakhare Nichael <handzome4u@yahoo.com>
2022-11-09 21:05:08 +0000

    Set all fragment inside the chatActivity and declaration of variables to work inside the chatActivity.

--- app/src/main/java/com/example/tuchattingapp/chatActivity.java
+++ app/src/main/java/com/example/tuchattingapp/chatActivity.java
@@ -1,15 +1,107 @@
 package com.example.tuchattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class chatActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem mchat,mstatus,mroom,mcall;
+    ViewPager viewPager;
+    PagerAdapter pagerAdapter;
+    androidx.appcompat.widget.Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        tabLayout=findViewById(R.id.include);
+        mchat=findViewById(R.id.chat);
+        mstatus=findViewById(R.id.status);
+        mroom=findViewById(R.id.room);
+        mcall=findViewById(R.id.calls);
+        viewPager=findViewById(R.id.fragment_container);
+
+        mtoolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+
+        Drawable drawable= ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_baseline_more_vert_24);
+        mtoolbar.setOverflowIcon(drawable);
+
+
+        pagerAdapter=new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if(tab.getPosition()==0 || tab.getPosition()==1 || tab.getPosition()==2 || tab.getPosition()==3)
+                {
+                    pagerAdapter.notifyDataSetChanged();
+                }
+
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
-    
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+       switch (item.getItemId())
+       {
+           case R.id.profile:
+               Intent intent=new Intent( chatActivity.this,ProfileActivity.class);
+               startActivity(intent);
+               break;
+           case R.id.settings:
+               Toast.makeText(getApplicationContext(),"settings",Toast.LENGTH_SHORT).show();
+               break;
+       }
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return true;
+    }
 }
